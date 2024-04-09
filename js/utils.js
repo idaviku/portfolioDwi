8bffec8b4d01bb339ca86b9e1c49950ca0969d43
Dawi <xnk.davik@gmail.com>
2024-04-09 00:43:54 -0500

    change implementation of apisUrl object 4 fetch requests

--- js/utils.js
+++ js/utils.js
@@ -2,6 +2,15 @@
       contenido: utilidades, funciones auxiliares globales
       (ayuda, constantes, configuraciones)
 */
+const apisUrl = {
+    github:{
+        user_url:'https://api.github.com/users/idaviku',
+        repos_url:'https://api.github.com/users/idaviku/repos',
+        gists_url:'https://api.github.com/users/idaviku/gists',
+        events_url:'https://api.github.com/users/idaviku/events'
+    },
+    dataJson:'../data-cv.json'
+};
 
 async function fetchData(url) {
   try {
