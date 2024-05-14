c210d5797e90b519052d40f336a6b11c8c5f995c
Dawi <xnk.davik@gmail.com>
2024-05-13 23:17:46 -0500

    change dark mode design

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const seeMenu = document.querySelector('.menu')
 
 function cambiarEstado(){
   document.querySelector('.nav__menu__container').classList.toggle('visuallyHidden');
+  document.querySelector('.display__blur').classList.toggle('visuallyHidden');
 }
 seeMenu.onclick = () => {
   cambiarEstado();
