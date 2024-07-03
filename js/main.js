219f88e059c5f962d69e899338d8bc33dc67845a
Dawi <xnk.davik@gmail.com>
2024-07-03 00:40:51 -0500

    Add functionality for custom printing of cv

--- js/main.js
+++ js/main.js
@@ -1,54 +1,58 @@
 
 const seeMenu = document.querySelector('.menu')
-const displayBlur=document.querySelector('.display__blur')
+const displayBlur = document.querySelector('.display__blur')
 //const menu=document.querySelector('.nav__menu__container')
 //const listaul=document.querySelector('.nav__container__items')
-function cambiarEstado(){
+const inputMenu=document.querySelector('.nav__header__init')
+const printFormat=document.querySelector('#printFormat')
+
+function cambiarEstado() {
   document.querySelector('.nav__menu__container').classList.toggle('visuallyHidden');
   displayBlur.classList.toggle('visuallyHidden');
+  inputMenu.classList.toggle('visuallyHidden')
 }
 seeMenu.onclick = () => {
   cambiarEstado();
-} 
+}
 /* seeMenu.addEventListener("click",()=>{
   cambiarEstado();
 }) */
 document.addEventListener('DOMContentLoaded', function () {
-  const menu=document.querySelector('.nav__menu__container')
-  let isMenuOpen=false
-  document.addEventListener('keydown',function (e) {
-    if ((e.ctrlKey && e.key === 'k')|| e.key==='Escape') {
+  const menu = document.querySelector('.nav__menu__container')
+  let isMenuOpen = false
+  document.addEventListener('keydown', function (e) {
+    if ((e.ctrlKey && e.key === 'k') || e.key === 'Escape') {
       e.preventDefault()
-      isMenuOpen? closeMenu():openMenu()      
+      isMenuOpen ? closeMenu() : openMenu()
     }
   })
   function openMenu() {
     cambiarEstado()
-    isMenuOpen=true
-    document.addEventListener('keydown',handleMenuNavigation)
+    isMenuOpen = true
+    document.addEventListener('keydown', handleMenuNavigation)
   }
   function closeMenu() {
     cambiarEstado()
-    isMenuOpen=false
-    document.addEventListener('keydown',handleMenuNavigation)
+    isMenuOpen = false
+    document.addEventListener('keydown', handleMenuNavigation)
   }
-  displayBlur.addEventListener('click',closeMenu)
+  displayBlur.addEventListener('click', closeMenu)
 
   function handleMenuNavigation(e) {
-    const currentFocus=document.activeElement
-    const menuItems=menu.querySelectorAll('.nav__item__a')
-    let currentIndex=Array.from(menuItems).indexOf(currentFocus)
-    let nextIndex=-1
+    const currentFocus = document.activeElement
+    const menuItems = menu.querySelectorAll('.nav__item__a')
+    let currentIndex = Array.from(menuItems).indexOf(currentFocus)
+    let nextIndex = -1
 
     switch (e.key) {
       case 'ArrowUp':
-          nextIndex=currentIndex > 0 ? currentIndex - 1: menuItems.length - 1        
+        nextIndex = currentIndex > 0 ? currentIndex - 1 : menuItems.length - 1
         break;
       case 'ArrowDown':
-          nextIndex=currentIndex < menuItems.length - 1 ? currentIndex + 1 : 0
+        nextIndex = currentIndex < menuItems.length - 1 ? currentIndex + 1 : 0
         break;
       case 'Enter':
-          menuItems[currentIndex].click()
+        menuItems[currentIndex].click()
         break;
     }
     if (nextIndex !== -1) {
@@ -58,15 +62,27 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 })
 function openModal(modalId) {
-  const modal=document.querySelector(`.${modalId}`)
+  const modal = document.querySelector(`.${modalId}`)
   if (modal) {
     modal.showModal();
   }
 }
 function closeModal(modalId) {
-  const modal=document.querySelector(`.${modalId}`)
+  const modal = document.querySelector(`.${modalId}`)
   if (modal) {
     modal.close();
   }
 }
-
+function printCurriculum(reportPrint) {
+  const impresion=document.querySelector(reportPrint)
+  impresion.classList.toggle('print__section')
+  printFormat.href='./css/printformatcv.css'
+  setTimeout(() => {
+    window.print()
+    impresion.classList.toggle('print__section')
+  }, 500
+);
+}
+window.addEventListener("afterprint", (event) => {
+  printFormat.href='./css/print.css'
+});
