1e2462d0c945194b3ec201b5631903c559fa6406
davik <xnk.davik@gmail.com>
2024-12-02 15:32:45 -0500

    fix bug where search in menu filter did not work

--- js/main.js
+++ js/main.js
@@ -6,48 +6,66 @@ const displayBlur = document.querySelector('.display__blur')
 const inputMenu=document.querySelector('.nav__header__init')
 const printFormat=document.querySelector('#printFormat')
 const searchInput=document.querySelector('.searchInput')
+const menu1 = document.querySelector('.nav__menu__container')
+const menu2 = document.querySelector('.nav__container__logo')
+const itemsMenu1 = menu1.querySelectorAll('.nav__item__a')
 
+//if ('serviceWorker' in navigator) {
+  //window.addEventListener('load', () => {
+    //navigator.serviceWorker.register('js/service-worker.js')
+      //.then(registration => {
+        //console.log('Service Worker registrado con éxito:', registration);
+      //})
+      //.catch(error => {
+        //console.log('Error al registrar el Service Worker:', error);
+      //});
+  //});
+//}
 
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('js/service-worker.js')
-      .then(registration => {
-        console.log('Service Worker registrado con éxito:', registration);
-      })
-      .catch(error => {
-        console.log('Error al registrar el Service Worker:', error);
-      });
-  });
-}
 function cambiarEstado() {
   document.querySelector('.nav__menu__container').classList.toggle('visuallyHidden');
   displayBlur.classList.toggle('visuallyHidden');
   inputMenu.classList.toggle('visuallyHidden')
   searchInput.focus()
-  //const menuItems = menu.querySelectorAll('.nav__item__a')
-  //searchInput.addEventListener('input', ()=>{
-    //const filter=searchInput.value.toLowerCase()
-    //menuItems.forEach(e=>{
-      //const text=e.textContent.toLowerCase()
-      //if (text.includes(filter)) {
-        //e.focus()
-      //} 
-    //})
-  //})
 }
+
 seeMenu.onclick = () => {
   cambiarEstado();
 }
 inputMenu.onclick = () => {
   cambiarEstado();
 }
-//observer.observe(seeMenu)
-/* seeMenu.addEventListener("click",()=>{
-  cambiarEstado();
-}) */
+
+function filterMenu(query, items) {
+  for (let i = 0; i < items.length; i++) {
+    const item = items[i];
+    const text = item.textContent.toLowerCase();
+
+    if (text.includes(query)) {
+      item.style.display = ''; 
+    } else {
+      item.style.display = 'none'; 
+    }
+  }
+}
+
+searchInput.addEventListener('input', function() {
+  const query = searchInput.value.toLowerCase();
+  filterMenu(query, itemsMenu1)
+  //filterMenu(query, itemsMenu2);
+  //selectFirstVisibleOption()
+});
+
+function selectFirstVisibleOption() {
+  const visibleItems = [...itemsMenu1].filter(item => item.style.display !== 'none');
+
+  if (visibleItems.length > 0) {
+    visibleItems[0].focus(); 
+    visibleItems[0].scrollIntoView({ behavior: 'smooth', block: 'center' }); 
+  }
+}
+
 document.addEventListener('DOMContentLoaded', function () {
-  const menu = document.querySelector('.nav__menu__container')
   let isMenuOpen = false
   document.addEventListener('keydown', function (e) {
     if ((e.ctrlKey && e.key === 'k') || e.key === 'Escape') {
@@ -69,39 +87,43 @@ document.addEventListener('DOMContentLoaded', function () {
 
   function handleMenuNavigation(e) {
     const currentFocus = document.activeElement
-    const menuItems = menu.querySelectorAll('.nav__item__a')
-    let currentIndex = Array.from(menuItems).indexOf(currentFocus)
+    const visibleItems=[...menu1.querySelectorAll('.nav__item__a')].filter(e => e.style.display !== 'none')
+    let currentIndex = visibleItems.indexOf(currentFocus)
     let nextIndex = -1
 
     switch (e.key) {
       case 'ArrowUp':
-        nextIndex = currentIndex > 0 ? currentIndex - 1 : menuItems.length - 1
+        nextIndex = currentIndex > 0 ? currentIndex - 1 : visibleItems.length - 1
         break;
       case 'ArrowDown':
-        nextIndex = currentIndex < menuItems.length - 1 ? currentIndex + 1 : 0
+        nextIndex = currentIndex < visibleItems.length - 1 ? currentIndex + 1 : 0
         break;
       case 'Enter':
-        menuItems[currentIndex].click()
+        selectFirstVisibleOption()
+        visibleItems[currentIndex].click()
         break;
     }
     if (nextIndex !== -1) {
-      menuItems[nextIndex].focus()
+      visibleItems[nextIndex].focus()
       e.preventDefault()
     }
   }
 })
+
 function openModal(modalId) {
   const modal = document.querySelector(`.${modalId}`)
   if (modal) {
     modal.showModal();
   }
 }
+
 function closeModal(modalId) {
   const modal = document.querySelector(`.${modalId}`)
   if (modal) {
     modal.close();
   }
 }
+
 function printCurriculum(reportPrint) {
   const impresion=document.querySelector(reportPrint)
   impresion.classList.toggle('print__section')
@@ -112,6 +134,7 @@ function printCurriculum(reportPrint) {
   }, 500
 );
 }
+
 window.addEventListener("afterprint", () => {
   printFormat.href='./css/print.css'
 });
